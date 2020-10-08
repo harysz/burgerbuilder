d2599730bb4a7cab715293deebfd67f1cecafe27
harysz <arnodlas.valasinas@gmail.com>
2020-10-08 22:39:36 +0300

    added dynamical rendering Obj to array map

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,23 @@ import Burger from '../../components/Burger/Buger';
 
 
 class BurgerBuilder extends Component {
- render (){
+//  constructor(props){
+//    super(props);
+//    this.state={...}
+//  }
+  state ={
+    ingredients :{
+      salad:0,
+      bacon:0,
+      cheese :0,
+      meat:0
+    }
+  }
+ 
+  render (){
      return (
        <Aux>
-            <Burger/>
+            <Burger ingredients={this.state.ingredients}/>
             <div>Builder Controls</div>
        </Aux>
      );
