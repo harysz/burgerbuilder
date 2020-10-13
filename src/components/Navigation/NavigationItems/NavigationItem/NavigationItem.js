e62b6721c3ddfe0798094fbf81175ba211d88d9d
harysz <arnodlas.valasinas@gmail.com>
2020-10-13 20:48:05 +0300

    adding links to Toolbar

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const navigationItem = () =>{
+    <li><a href=''> A link</a></li>
+}
+export default navigationItem;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const navigationItems=(props)=> {
+    return (
+        <ul>
+            
+        </ul>
+    )
+}
+export default navigationItems;
