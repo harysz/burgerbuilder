04c1a70faacdbc283cb4de3fe6c4f25f61c6b7ae
harysz <arnodlas.valasinas@gmail.com>
2020-10-14 19:58:23 +0300

    added responsive inline styles for logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import Aux from '../../hoc/Auxilary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 const layout = ( props )=>(
     <Aux>
        <Toolbar />
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import logoImg from '../../assets/burger-logo.png';
 import classes from './Logo.css';
 const logo=(props)=> {
     return (
-        <div className={classes.Logo}>
+        <div className={classes.Logo} style={{height:props.height}}>
             <img src={logoImg} alt = 'no logo makes me loco'/>
         </div>
     )
