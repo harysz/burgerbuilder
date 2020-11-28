df13aef3a10d2b9a31624d9325087b5753f29ece
harysz <arnodlas.valasinas@gmail.com>
2020-11-28 13:20:05 +0200

    added menu button fixed backdrop functionality

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,11 +3,11 @@ import React from 'react'
 import Logo from '../../Logo/Logo';
 import classes from './Toolbar.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
-
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 const toolbar=(props)=> {
     return (
         <header className={classes.Toolbar}>
-            <div>Menu</div>
+            <DrawerToggle clicked={props.drawerToggleClicked}/>
             <Logo height='80%'/>
             <nav className={classes.DesktopOnly}>
                 <NavigationItems />
