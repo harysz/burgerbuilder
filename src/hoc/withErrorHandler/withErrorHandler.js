028e0ca231c97871f4e4c59357992846dbca76c3
harysz <arnodlas.valasinas@gmail.com>
2021-01-06 19:26:55 +0200

    memory leaks fixed

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,15 +11,18 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
         }
 
         componentWillMount () {
-            axios.interceptors.request.use(req => {
+            this.reqInteceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
         }
-
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.reqInteceptor);
+            axios.interceptors.request.eject(this.resInterceptor);
+        }
         errorConfirmedHandler = () => {
             this.setState({error: null});
         }
